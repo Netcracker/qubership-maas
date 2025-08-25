55a22a171331a928621af9b8b8c43cd4c9b122ac
Nurtay Tolymbek <nurtay.tolymbek@netcracker.com>
2025-08-25 17:46:14 +0500

    fix(test): stabilize token_source tests using select loop

--- maas/maas-service/kubernetes/oidc/token_source.go
+++ maas/maas-service/kubernetes/oidc/token_source.go
@@ -18,7 +18,7 @@ type fileTokenSource struct {
 	tokenDir string
 }
 
-func NewFileTokenSource (ctx context.Context, tokenDir string) (*fileTokenSource, error) {
+func NewFileTokenSource(ctx context.Context, tokenDir string) (*fileTokenSource, error) {
 	ts := &fileTokenSource{
 		logger:   logging.GetLogger("oidc.fileTokenSource"),
 		tokenDir: tokenDir,
@@ -32,7 +32,7 @@ func NewFileTokenSource (ctx context.Context, tokenDir string) (*fileTokenSource
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize file watcher: %w", err)
 	}
-	err = watcher.Add(ts.tokenDir)
+	err = watcher.Add(ts.tokenDir + "/")
 	if err != nil {
 		return nil, fmt.Errorf("failed to add path %s to file watcher: %w", ts.tokenDir, err)
 	}
@@ -48,24 +48,26 @@ func (f *fileTokenSource) Token() (string, error) {
 	return f.token, nil
 }
 
-func (f *fileTokenSource) listenFs(ctx context.Context,events chan fsnotify.Event, errs chan error) {
-	select {
-	case ev := <-events:
-		// we look for event "..data file created". kubernetes updates the token by updating the "..data" symlink token file points to.
-		if path.Base(ev.Name) == "..data" && ev.Op.Has(fsnotify.Create) {
-			f.logger.Infof("volume mounted token updated, refreshing token at dir %s", f.tokenDir)
-			f.mu.Lock()
-			err := f.refreshToken()
-			f.mu.Unlock()
-			if err != nil {
-				f.logger.Errorf("watching volume token at dir %s: %w", f.tokenDir, err)
+func (f *fileTokenSource) listenFs(ctx context.Context, events chan fsnotify.Event, errs chan error) {
+	for {
+		select {
+		case ev := <-events:
+			// we look for event "..data file created". kubernetes updates the token by updating the "..data" symlink token file points to.
+			if path.Base(ev.Name) == "..data" && ev.Op.Has(fsnotify.Create) {
+				f.logger.Infof("volume mounted token updated, refreshing token at dir %s", f.tokenDir)
+				f.mu.Lock()
+				err := f.refreshToken()
+				f.mu.Unlock()
+				if err != nil {
+					f.logger.Errorf("watching volume token at dir %s: %w", f.tokenDir, err)
+				}
 			}
-		}
-	case err := <-errs:
-		f.logger.Errorf("error at volume mounted token watcher at path %s: %w", f.tokenDir, err)
-	case <-ctx.Done():
+		case err := <-errs:
+			f.logger.Errorf("error at volume mounted token watcher at path %s: %w", f.tokenDir, err)
+		case <-ctx.Done():
 			f.logger.Infof("token watcher at %s shutdown", f.tokenDir)
-		return
+			return
+		}
 	}
 }
 
