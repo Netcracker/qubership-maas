72c4ff3a3d1b20869f3795299a4eb445b43eb2ae
Nurtay Tolymbek <nurtay.tolymbek@netcracker.com>
2025-08-25 15:27:48 +0500

    test new file token source that watches file system

--- maas/maas-service/kubernetes/oidc/token_source_test.go
+++ maas/maas-service/kubernetes/oidc/token_source_test.go
@@ -2,7 +2,6 @@ package oidc
 
 import (
 	"os"
-	"sync"
 	"testing"
 	"time"
 
@@ -12,82 +11,69 @@ import (
 var logger = logging.GetLogger("server")
 
 func TestFileTokenSource(t *testing.T) {
-	f, err := os.CreateTemp(t.TempDir(), "")
+	tokenDir := t.TempDir()
+	tokenFilePath := tokenDir + "/token"
+	dataSymlinkPath := tokenDir + "/..data"
+	tokenFile, err := os.CreateTemp(tokenDir, "")
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer f.Close()
-	newToken := "new_token"
-	stillValidToken := "still_valid_token"
-	expiredToken := "expired_token"
-	tests := []struct {
-		name    string
-		oldTok  string
-		fTok    string
-		expiry  time.Time
-		now     func() time.Time
-		wantTok string
-	}{
-		{
-			name:    "token still valid. return old token",
-			oldTok:  stillValidToken,
-			expiry:  time.Now().Add(time.Minute),
-			now:     func() time.Time { return time.Now() },
-			wantTok: stillValidToken,
-		},
-		{
-			name:    "token expired re read from file",
-			oldTok:  expiredToken,
-			fTok:    newToken,
-			expiry:  time.Now(),
-			now:     func() time.Time { return time.Now() },
-			wantTok: newToken,
-		},
+	defer tokenFile.Close()
+	err = os.Symlink(tokenFile.Name(), dataSymlinkPath)
+	if err != nil {
+		t.Fatal(err)
 	}
-	for _, test := range tests {
-		if test.fTok != "" {
-			_, err := f.WriteAt([]byte(test.fTok), 0)
-			if err != nil {
-				t.Fatal(err)
-			}
-		}
-		fts := newFileTokenSource(logger, f.Name(), test.now)
-		fts.expiry = test.expiry
-		fts.token = test.oldTok
-		token, err := fts.Token()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if test.wantTok != token {
-			t.Errorf("test %q: expected token %s, got %s", test.name, test.wantTok, token)
-		}
+	err = os.Symlink(dataSymlinkPath, tokenFilePath)
+	if err != nil {
+		t.Fatal(err)
 	}
-}
 
-func TestFileTokenSourceRace(t *testing.T) {
-	f, err := os.CreateTemp(t.TempDir(), "")
+	firstValidToken := "first_valid_token"
+	_, err = tokenFile.Write([]byte(firstValidToken))
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer f.Close()
-	fts := newFileTokenSource(logger, f.Name(), nil)
-	var wg sync.WaitGroup
-	errCh := make(chan error)
-	done := make(chan bool)
-	for range 10 {
-		wg.Add(1)
-		go func() {
-			_, err := fts.Token()
-			if err != nil {
-				errCh <- err
-			}
-			wg.Done()
-		}()
-	}
-	go func() { wg.Wait(); done <- true }()
-	select {
-	case err := <-errCh:
+
+	fts, err := newFileTokenSource(logger, tokenDir)
+	if err != nil {
 		t.Fatal(err)
-	case <-done:
+	}
+	token, err := fts.Token()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if firstValidToken != token {
+		t.Errorf("expected token %s, got %s", firstValidToken, token)
+	}
+
+	secondValidToken := "second_valid_token"
+	_, err =tokenFile.WriteAt([]byte(secondValidToken), 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.Remove(dataSymlinkPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.Remove(tokenFilePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.Symlink(tokenFile.Name(), dataSymlinkPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.Symlink(dataSymlinkPath, tokenFilePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	time.Sleep(time.Millisecond*100)
+	token, err = fts.Token()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if secondValidToken != token {
+		t.Errorf("expected token %s, got %s", secondValidToken, token)
 	}
 }
