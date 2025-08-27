8f4a00882b2c9645a88c4b94d3ed024a4e651702
Nurtay Tolymbek <nurtay.tolymbek@netcracker.com>
2025-08-27 12:08:28 +0500

    oidc: remove kubernetes.oidc.tokendir config value

--- maas/maas-service/kubernetes/oidc/token_source.go
+++ maas/maas-service/kubernetes/oidc/token_source.go
@@ -11,6 +11,8 @@ import (
 	"github.com/netcracker/qubership-core-lib-go/v3/logging"
 )
 
+const serviceAccountDir = "/var/run/secrets/kubernetes.io/serviceaccount"
+
 type fileTokenSource struct {
 	mu       sync.RWMutex
 	logger   logging.Logger
@@ -18,7 +20,11 @@ type fileTokenSource struct {
 	tokenDir string
 }
 
+// if tokenDir == "", default /var/run/secrets/kubernetes.io/serviceaccount/token is used
 func NewFileTokenSource(ctx context.Context, tokenDir string) (*fileTokenSource, error) {
+	if tokenDir == "" {
+		tokenDir = serviceAccountDir
+	}
 	ts := &fileTokenSource{
 		logger:   logging.GetLogger("oidc.fileTokenSource"),
 		tokenDir: tokenDir,
