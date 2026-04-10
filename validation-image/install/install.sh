0668389b631589ca0eeb2bca13e5c34934b6c6fe
Sergei Voronov <sergei.voronov@netcracker.com>
2026-04-10 15:48:21 +0800

    chore: update base image versions and vulnerabils

--- validation-image/install/install.sh
+++ validation-image/install/install.sh
@@ -2,6 +2,7 @@
 set -e
 set -x
 
+apk upgrade --no-cache
 apk add --no-cache zip unzip jq postgresql wget curl bash python3
 
 mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
@@ -14,7 +15,7 @@ chmod +x /bootstrap.sh
 chmod -R +x /scripts
 
 GSDK=https://dl.k8s.io/release
-curl -L "${GSDK}"/v1.34.1/bin/linux/amd64/kubectl  -o /bin/kubectl
+curl -L "${GSDK}"/v1.34.2/bin/linux/amd64/kubectl  -o /bin/kubectl
 chmod +x /bin/kubectl*
 
 exit 0
